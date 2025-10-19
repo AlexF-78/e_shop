31da1424a0ac4f8d0e66b51cd06b916d60b7e415
алексей федулов <fab-78@mail.ru>
2025-10-19 20:31:37 +0200

    corrected models.py

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     """Модель категории товаров"""
 
-    objects = None
+
     name = models.CharField(
         max_length=100,
         verbose_name='наименование'
@@ -27,7 +27,7 @@ class Category(models.Model):
 class Product(models.Model):
     """Модель товара"""
 
-    objects = None
+
     name = models.CharField(
         max_length=100,
         verbose_name='наименование'
