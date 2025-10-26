50fc4127e8fc0a1165c1534da4424315e57ad50d
алексей федулов <fab-78@mail.ru>
2025-10-26 22:54:35 +0200

    corrected views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from .models import Product, Category
+
 
 def home(request):
-    return render(request, 'home.html')
+    # Получаем все товары для отображения на Главной
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
 
 def contact(request):
     return render(request, 'contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
+
