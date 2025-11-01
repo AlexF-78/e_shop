18d2a84395ff70ffc47ba93ce28653148056e872
алексей федулов <fab-78@mail.ru>
2025-11-02 00:50:03 +0200

    corrected settings.py, urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),  # 'catalog'
+    path('', include('catalog.urls')), # 'catalog'
+    path('blog/', include('blog.urls')),  # 'blog'
 ]
 
 if settings.DEBUG:
