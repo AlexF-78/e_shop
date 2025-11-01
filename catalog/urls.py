f851cf24bd36855ca8965ad6c1ef4a15c0c39663
алексей федулов <fab-78@mail.ru>
2025-11-02 00:46:22 +0200

    Created catalog\views.py, urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,31 @@
+# #from django import views
+# from django.urls import path
+# #from django.views.generic import DetailView
+#
+# from catalog.apps import CatalogConfig
+# from catalog.views import HomeView, ContactView, ProductView
+# # from . import views
+#
+# app_name = CatalogConfig.name
+#
+# urlpatterns = [
+#     #path('', views.home, name='home'),
+#     path('', HomeView.as_view(), name='home'),
+#     # path('contacts/', views.contact, name='contact'),
+#     path('', ContactView.as_view(), name='contact'),
+#     # path('product/<int:pk>/', views.product_detail, name='product_detail'),
+#     path('', DetailView.as_view, name='product'),
+#     path('product/<int:pk>/', views.ProductView.as_view(), name='product_detail'),
+# ]
+
 
 from django.urls import path
-from catalog.apps import CatalogConfig
-from catalog.views import home
 from . import views
 
-app_name = CatalogConfig.name
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contact, name='contact'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('product/<int:pk>/', views.ProductView.as_view(), name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import get_object_or_404,render
+
+# from django.shortcuts import get_object_or_404,render
+# from django.views import View
+from django.views.generic import ListView, TemplateView, DetailView
+
 from .models import Product, Category
 
-
-def home(request):
-    # Получаем все товары для отображения на Главной
-    products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
-
-def contact(request):
-    return render(request, 'contacts.html')
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product,pk=pk)
-    return render(request, 'product_detail.html', {'product': product})
+class HomeView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'products'
 
 
+# def home(request):
+#     # Получаем все товары для отображения на Главной
+#     products = Product.objects.all()
+#     return render(request, 'home.html', {'products': products})
+
+class ContactView(TemplateView):
+    template_name = 'contacts.html'
+
+# def contact(request):
+#     return render(request, 'contacts.html')
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product,pk=pk)
+#     return render(request, 'product_detail.html', {'product': product})
