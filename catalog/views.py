bf8db7b26ea872c10e76536e2556ca482b8afa76
алексей федулов <fab-78@mail.ru>
2025-11-09 23:32:20 +0200

    corrected views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,19 @@
-
+from django.contrib import messages
+from django.urls import reverse_lazy
 # from django.shortcuts import get_object_or_404,render
 # from django.views import View
-from django.views.generic import ListView, TemplateView, DetailView
-
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import ProductForm
 from .models import Product, Category
 
 class HomeView(ListView):
     model = Product
     template_name = 'home.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        """Оптимизируем запрос, загружая связанные категории"""
+        return Product.objects.all()
 
 
 # def home(request):
@@ -27,6 +32,82 @@ class ProductView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
+
+    def get_queryset(self):
+        """Оптимизируем запрос, загружая связанные категории"""
+        return Product.objects.all()
+
+
+class ProductListView(ListView):
+    """Список всех товаров"""
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('name')
+
+
+class ProductCreateView(CreateView):
+    """Создание нового товара"""
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Создать новый товар'
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, f'Товар "{self.object.name}" успешно создан!')
+        return response
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Пожалуйста, исправьте ошибки в форме.')
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    """Редактирование товара"""
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Редактировать товар: {self.object.name}'
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, f'Товар "{self.object.name}" успешно обновлен!')
+        return response
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Пожалуйста, исправьте ошибки в форме.')
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    """Удаление товара"""
+    model = Product
+    template_name = 'product_confirm_delete.html'
+    context_object_name = 'product'
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        product_name = self.object.name
+        response = super().form_valid(form)
+        messages.success(self.request, f'Товар "{product_name}" успешно удален!')
+        return response
 
 # def product_detail(request, pk):
 #     product = get_object_or_404(Product,pk=pk)
